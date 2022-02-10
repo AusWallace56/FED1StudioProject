f3e040c6f7475de68221cbad86a09b531d574905
Austin Wallace <auswallace@outlook.com>
2022-02-09 19:30:08 -0600

    Style changes

---
+++ FED1StudioProject/JS/teampics.js
@@ -0,0 +1,17 @@
+﻿
+$(function () {
+
+
+    $(".teamImage").on("click", function () {
+
+        var $imgSrc = $(this).attr("src");
+
+        $("lightbox-content").html('<img class="teamImage" src="' + $imgSrc + '"/>');
+
+        $("#lightbox-container").fadeIn(300);
+
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(300);
+        });
+    });
+});
